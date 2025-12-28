2fc576f71a6f52323fcf287b647e5a4e61328a3c
adminpomme <admin.pomme@gmail.com>
2025-12-28 09:28:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,10 @@
-Bash
-
 #!/bin/bash
-# 1. 修改默认 IP 为 192.168.10.1 (防光猫冲突)
-sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
+# 1. 修改默认 IP 为 192.168.10.1
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# 2. 扩容：设定 1600MB 根分区 (确保安装后有 >1GB 空闲)
-sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=1600/g' openwrt/.config
+# 2. 强制分区大小 (1600MB 以确保 1G 空闲空间)
+# 注意：在执行此脚本前 .config 已经存在于当前目录
+sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=1600/g' .config
 
-# 3. 移除重复或冲突的源 (可选)
-# sed -i 's/something/something_else/g' ...
+# 3. 设置固件名称标识
+sed -i 's/OpenWrt/Official-24.10-Custom/g' package/base-files/files/etc/banner
