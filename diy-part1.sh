31fbdf8805c604a0f36b42d6f77691b00b9f4679
adminpomme <admin.pomme@gmail.com>
2025-12-28 09:36:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# 添加 PassWall 及其依赖源
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> openwrt/feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> openwrt/feeds.conf.default
-
-# 添加 MosDNS 源码
-echo "src-git mosdns https://github.com/Iinex/luci-app-mosdns.git;main" >> openwrt/feeds.conf.default
-
-# 添加包含 AdGuardHome 的综合插件源
-echo "src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master" >> openwrt/feeds.conf.default
+# 进入 openwrt 目录操作 feeds 配置文件
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' openwrt/feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' openwrt/feeds.conf.default
+sed -i '$a src-git mosdns https://github.com/Iinex/luci-app-mosdns.git;main' openwrt/feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' openwrt/feeds.conf.default
