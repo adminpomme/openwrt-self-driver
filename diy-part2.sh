38701ac6370e7b8fb4bed766d8b481f642d3969a
adminpomme <admin.pomme@gmail.com>
2025-12-28 18:50:39 +0800

    Update IP, partition size, and banner in script

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# 1. 修改默认 IP 为 192.168.10.1
+# 注意：此脚本在 openwrt 目录下执行
+
+# 1. 修改默认 IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# 2. 强制分区大小 (1600MB，系统占约 400MB，剩下 1.2G 给软件)
+# 2. 扩容 1600MB
 sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=1600/g' .config
 
-# 3. 开启 IPv6 默认支持
+# 3. 开启 IPv6
 sed -i 's/0/1/g' package/network/config/ipv6helper/files/etc/config/network 2>/dev/null || true
 
-# 4. 修改固件横幅标识
-sed -i 's/OpenWrt/Official-24.10-Full-v6/g' package/base-files/files/etc/banner
+# 4. 修改标识
+sed -i 's/OpenWrt/Official-24.10-Full/g' package/base-files/files/etc/banner
