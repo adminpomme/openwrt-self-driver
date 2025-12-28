d357366ae4de83fd1b4d5283d2272a4ed00a891c
adminpomme <admin.pomme@gmail.com>
2025-12-28 09:36:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# 1. 修改默认 IP 为 192.168.10.1
+# 1. 修改默认 IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# 2. 强制分区大小 (1600MB 以确保 1G 空闲空间)
-# 注意：在执行此脚本前 .config 已经存在于当前目录
+# 2. 扩容：设定 1600MB 根分区 (确保安装后有 >1GB 空闲空间)
 sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*$/CONFIG_TARGET_ROOTFS_PARTSIZE=1600/g' .config
 
-# 3. 设置固件名称标识
-sed -i 's/OpenWrt/Official-24.10-Custom/g' package/base-files/files/etc/banner
+# 3. 强制启用 IPv6 支持
+sed -i 's/0/1/g' package/network/config/ipv6helper/files/etc/config/network 2>/dev/null || true
+
+# 4. 修改固件标识
+sed -i 's/OpenWrt/Official-24.10-IPv6-Final/g' package/base-files/files/etc/banner
