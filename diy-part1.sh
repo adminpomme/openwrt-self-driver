c705271176875d6e6c3d84cddafc864057c42b3e
adminpomme <admin.pomme@gmail.com>
2025-12-28 09:39:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# 进入 openwrt 目录操作 feeds 配置文件
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' openwrt/feeds.conf.default
-sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' openwrt/feeds.conf.default
-sed -i '$a src-git mosdns https://github.com/Iinex/luci-app-mosdns.git;main' openwrt/feeds.conf.default
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' openwrt/feeds.conf.default
+# 进入 package 目录
+cd openwrt/package
+
+# 1. 强行拉取 PassWall (包括核心包)
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git passwall_deps
+
+# 2. 强行拉取 MosDNS
+git clone --depth 1 https://github.com/Iinex/luci-app-mosdns.git
+
+# 3. 强行拉取 AdGuardHome 及常用插件源 (kiddin9)
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git extra_pkgs
